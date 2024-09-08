0f91023cc9e0f30664836d38f0a27a2fe5935443
Smustus <rikardfors_91@hotmail.com>
2024-09-09 00:30:28 +0200

    added code
    trying out openai api

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,18 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, convertToCoreMessages } from 'ai';
+
+//https://sdk.vercel.ai/docs/getting-started/nextjs-app-router
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: openai('gpt-3.5-turbo'),
+    messages: convertToCoreMessages(messages),
+  });
+
+  return result.toDataStreamResponse();
+}
