4cba78ea4e190fb9f31afc8cc23245be8c6925ea
Smustus <rikardfors_91@hotmail.com>
2024-09-16 12:11:29 +0200

    added code
    improved aesthetics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,73 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import { useEffect, useRef } from 'react';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+  const scrollDown = useRef<HTMLUListElement>(null)
 
-      <form onSubmit={handleSubmit}>
+  useEffect(() => {
+    const node = scrollDown.current;
+    if(node){
+      node.scrollTop = node.scrollHeight;
+    }
+  });
+
+  const formatString = (string: string) => {
+    return string.slice(0, 1).toUpperCase() + string.slice(1)
+  }
+  
+  return (
+    <main className="flex flex-col w-full max-w-md h-fit py-12 mx-auto my-4 bg-gray-50 shadow-lg rounded-lg">
+      <h2 className='text-center'>
+        AI-chatbot
+      </h2>
+      <ul ref={scrollDown} className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((m) => (
+          <>
+          {m.role === 'user' ? (
+            <li
+              key={m.id}
+              className={`whitespace-pre-wrap p-3 rounded-lg ${
+                'bg-blue-500 text-white self-end'
+                }`}>
+              <span className="font-semibold">
+                {`${formatString(m.role)}: `}
+              </span>
+              {m.content}
+            </li>
+          ) : (
+            <li
+            key={m.id}
+            className={`whitespace-pre-wrap p-3 rounded-lg ${
+                'bg-gray-200 text-gray-900 self-start'
+              }`}>
+            <span className="font-semibold">
+              {`${formatString(m.role)}: `}
+            </span>
+            {m.content}
+          </li>
+          )
+          }
+          </>
+          ))}
+        </ul>
+
+      <form onSubmit={handleSubmit} className="sticky bottom-0 flex p-4 bg-white shadow-inner">
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="flex-1 p-3 mr-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={input}
-          placeholder="Say something..."
+          placeholder="Ask something..."
           onChange={handleInputChange}
         />
+        <button
+          type="submit"
+          className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Send
+        </button>
       </form>
-    </div>
+    </main>
   );
 }
